afcae9b2a31558bdfda697f53e2d49c453be621b
lorenasiombalo4 <168449589+lorenaiombalo4@users.noreply.github.com>
2024-10-07 10:56:46 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Português',
+    'Para que serve a vírgula?',
+    'Para separar elementos de uma oração, mas também orações de um só período. '
 )
 
 criaCartao(
